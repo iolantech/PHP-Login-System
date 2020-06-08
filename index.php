c714297bf405911b9b25759ba2d2b27631bcc268
IolanTech <iolantech@gmail.com>
2020-06-08 00:09:07 -0300

    Adding ui-kit

--- index.php
+++ index.php
@@ -1,1 +1,44 @@
-<?php echo "Hello World!"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.3/dist/css/uikit.min.css" />
+    <title>PHP Login System</title>
+</head>
+<body>
+    
+ <div class="uk-section uk-container">
+    <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid="">
+        <form class="uk-form-stacked">
+
+            <div class="uk-margin">
+                <label class="uk-form-label" for="form-stacked-text">Email</label>
+                <div class="uk-form-controls">
+                    <input class="uk-input" id="form-stacked-text" type="email" placeholder="email@gmail.com" required='required'>
+                </div>
+            </div>
+
+            <div class="uk-margin">
+                <label class="uk-form-label" for="form-stacked-text">Password</label>
+                <div class="uk-form-controls">
+                    <input class="uk-input" id="form-stacked-text" type="password" placeholder="Your Password" required='required'>
+                </div>
+            </div>
+
+            <div class="uk-button uk-button-default" type="submit">Submit</div>
+
+        </form>
+
+    </div>   
+
+ </div>
+
+
+
+<!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.3/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.3/dist/js/uikit-icons.min.js"></script>
+</body>
+</html>
